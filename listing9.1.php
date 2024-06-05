b88d156e52c79d70eafb6e8552824f7f4f208a79
meowmeowisthat <153915812+meowmeowisthat@users.noreply.github.com>
2024-06-05 18:06:07 +0700

    Add files via upload

--- listing9.1.php
+++ listing9.1.php
@@ -1,11 +1,25 @@
-<?php
-    $gaji = 1000000; // Define the gross salary
-    $pajak = 0.1; // Define the tax rate (10%)
-    $thp = $gaji - ($gaji * $pajak); // Calculate the net salary after tax
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Program 9.1</title>
+</head>
+<body>
 
-    // Display the gross salary
-    echo "Gaji sebelum pajak = Rp. $gaji <br>";
-    
-    // Display the net salary
-    echo "Gaji yang dibawa pulang = Rp. $thp";
-?>
+    <form method="post">
+        <label for="gaji">Masukkan Gaji: </label>
+        <input type="number" id="gaji" name="gaji" required>
+        <input type="submit" value="Total">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $gaji = $_POST['gaji'];
+        $pajak = 0.1; 
+        $thp = $gaji - ($gaji * $pajak); 
+
+        echo "<p>Gaji sebelum pajak = Rp. " . number_format($gaji, 0, ',', '.') . "</p>";
+        echo "<p>Gaji yang dibawa pulang = Rp. " . number_format($thp, 0, ',', '.') . "</p>";
+    }
+    ?>
+</body>
+</html>
--- listing9.2.php
+++ listing9.2.php
@@ -1,11 +1,25 @@
-<?php
-    $a = 5;
-    $b = 4;
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Program 9.2</title>
+</head>
+<body>
+    <h1>Perbandingan True || False</h1>
+    <?php
+        $a = 5; //mendeklarasikan varibel bernama a dengan value 5
+        $b = 4; //mendeklarasikan varibel bernama b dengan value 4
 
-    echo "$a == $b ". ($a == $b);
-    echo "<br>$a != $b : ". ($a != $b);
-    echo "<br>$a > $b : ". ($a > $b);
-    echo "<br>$a < $b : ". ($a < $b);
-    echo "<br>($a == $b) && ($a > $b) : ". (($a != $b) && ($a > $b));
-    echo "<br>($a == $b) || ($a > $b) : ". (($a != $b) || ($a > $b));
-?>
+        //pakai fungsi echo untuk menampilkan a == b : T/F
+        // pakai var_export untuk mengembalikan nilai boolean string
+        echo "$a == $b : " . var_export($a == $b, true) . "<br>";
+        //apakah 5 != 4? T
+        echo "$a != $b : " . var_export($a != $b, true) . "<br>";
+        //apakah 5 > 4? T
+        echo "$a > $b : " . var_export($a > $b, true) . "<br>";
+        //apakah 5 < b? F
+        echo "$a < $b : " . var_export($a < $b, true) . "<br>";
+        echo "($a != $b) && ($a > $b) : " . var_export(($a != $b) && ($a > $b), true) . "<br>";
+        echo "($a != $b) || ($a > $b) : " . var_export(($a != $b) || ($a > $b), true) . "<br>";
+    ?>
+</body>
+</html>
